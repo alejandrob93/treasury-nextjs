2494cda4c8614fbc7fd27735503694df0dc6dd2a
alejandrob93 <83163965+alejandrob93@users.noreply.github.com>
2025-10-26 19:35:34 -0700

    Create layout.js

---
+++ app/app/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Treasury Dashboard',
+  description: 'Next.js project',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
